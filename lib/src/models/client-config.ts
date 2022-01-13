c19cb05435026b7915033b140a31a5aac267578f
Thivi <theviyanthan@outlook.com>
2022-01-13 16:16:31 +0530

    Generate sign-in-redirect URL using base URL and the login path.

--- lib/src/models/client-config.ts
+++ lib/src/models/client-config.ts
@@ -2,8 +2,6 @@ import { AuthClientConfig } from "@asgardeo/auth-node-sdk";
 
 export interface ExpressClientConfig extends Omit<AuthClientConfig, "signInRedirectURL" | "signOutRedirectURL"> {
     baseURL: string,
-    signInRedirectURL: string,
-    signOutRedirectURL: string,
     cookieConfig?: {
         maxAge?: number,
         httpOnly?: boolean,
