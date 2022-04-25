1a902d17869a4ef4b004532ab719bc8cbe1fc1ab
Thivi <theviyanthan@outlook.com>
2022-04-25 18:49:58 +0400

    Rename auth-node-sdk to auth-node

--- lib/src/models/client-config.ts
+++ lib/src/models/client-config.ts
@@ -15,7 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { AuthClientConfig } from '@asgardeo/auth-node-sdk';
+import { AuthClientConfig } from '@asgardeo/auth-node';
 
 export interface StrictExpressClientConfig {
     baseURL: string,
